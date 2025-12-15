a488612a118fc6c8a9d0f8596f2654f6b9b65728
Garzia <francesco.garzia@gmail.com>
2025-12-15 22:34:38 +0100

    Made changes to support EMT (Elapsed Move Time) and CLK (Clock) information in the generated PGN output

--- src/app/api/[id]/download/all/route.ts
+++ src/app/api/[id]/download/all/route.ts
@@ -8,8 +8,15 @@ import {
 import { fetchTournament, fetchIndexData, getGamesData } from '@/utils/api';
 import { generatePgn } from '@/utils/game';
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
+    const { searchParams } = new URL(request.url);
+    const includeEMT = searchParams.get('includeEMT') === 'true';
+    const includeClkAttr = searchParams.get('includeClkAttr') === 'true';
+    const includeClkComment = searchParams.get('includeClkComment') === 'true';
     const tournament = await fetchTournament(params.id);
     const roundsWithGames = getRoundsWithGames(tournament.rounds);
     const indexData = await fetchIndexData(params.id, roundsWithGames);
@@ -26,7 +33,10 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
       indexMap,
       gamesData,
       extendedGamesUrls,
-      lookupMap
+      lookupMap,
+      includeEMT,
+      includeClkAttr,
+      includeClkComment
     );
     const response = new NextResponse(pgns);
     response.headers.set(
